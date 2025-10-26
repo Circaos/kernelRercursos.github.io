f2f4e76c0bdaedf00c98b86105bda70c62d20ca8
Cristopher <cristopher.crj@gmail.com>
2025-10-26 14:24:16 -0500

    correccion base_url

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 export const API_CONFIG = {
    // Puedes cambiarlo fácilmente si el servidor cambia
   // BASE_URL: "http://192.168.1.44:1333",
-  BASE_URL: "http://192.168.18.32:3000",
-  // BASE_URL: "https://apiproviaspruebav1-production.up.railway.app",
+  // BASE_URL: "http://192.168.18.32:3000",
+  BASE_URL: "https://apiproviaspruebav1-production.up.railway.app",
   ENDPOINTS: {
     GET_INFO_FECHA: "/webInt/getInfoFechaProviasSession",
     GET_DATA_FILTRADA: "/webInt/getDataFiltradaSession",
